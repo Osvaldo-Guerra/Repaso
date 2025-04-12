498747f401e9a89c81cecb10c81d2ce0a80b8f5b
Osvaldo <osvaldoguerra9909@gmail.com>
2025-04-12 10:35:41 -0500

    segunda subida

--- Semana 3/Program.cs
+++ Semana 3/Program.cs
@@ -11,7 +11,7 @@ namespace Semana_3
         static void Main(string[] args)
         {
             Console.WriteLine("fhksfjskfkgk");
-
+            Console.WriteLine("hdfhkshfkshfkf");
         }
     }
 }
